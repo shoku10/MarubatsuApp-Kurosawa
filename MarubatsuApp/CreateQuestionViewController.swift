77353cad621b6c34d06c79a384d8a5856bd23057
shoku10 <shoku8544@gmail.com>
2023-09-04 12:08:07 +0900

    UIをview controllerに接続

--- MarubatsuApp/CreateQuestionViewController.swift
+++ MarubatsuApp/CreateQuestionViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class CreateQuestionViewController: UIViewController {
-
+    
+    @IBOutlet weak var backTopButton: UIButton!
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    
+    @IBOutlet weak var answerSegmentedControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveQuestionButtonTapped(_ sender: UIButton) {
     }
-    */
-
+    
+    
+    @IBAction func questionDeleteButtonTapped(_ sender: UIButton) {
+    }
+    
 }
